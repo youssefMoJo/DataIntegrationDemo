b7b0856e3237f335741bba09e0ae84acc0454bd9
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2024-11-07 09:40:18 -0330

    Implemented insertBook method in BookRepository for adding book records to the database.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,15 +1,14 @@
 package main.java;
 
-import main.java.config.DatabaseConnection;
 import main.java.model.Book;
+import main.java.repository.BookRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Book book = new Book(1, "The Great Gatsby", "1925-04-10", 1);
+        BookRepository bookRepository = new BookRepository();
 
+        Book book = new Book(1, "Youssef", "1925-04-10", 1);
 
-        System.out.println(DatabaseConnection.getConnection());
-        System.out.println(book.getTitle());
-        
+        bookRepository.insertBook(book);
     }
 }
---
+++ src/main/java/repository/BookRepository.java
@@ -0,0 +1,41 @@
+package main.java.repository;
+
+
+import main.java.model.Book;
+import main.java.config.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class BookRepository {
+    public void insertBook(Book book) {
+        String query = "INSERT INTO books (title, publication_date, author_id) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, book.getTitle());
+            stmt.setString(2, book.getPublicationDate());
+            stmt.setInt(3, book.getAuthorId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public Book getBookById(int id) {
+    //     String query = "SELECT * FROM books WHERE id = ?";
+    //     try (Connection conn = DatabaseConnection.getConnection();
+    //          PreparedStatement stmt = conn.prepareStatement(query)) {
+    //         stmt.setInt(1, id);
+    //         ResultSet rs = stmt.executeQuery();
+    //         if (rs.next()) {
+    //             return new Book(rs.getInt("id"), rs.getString("title"), rs.getString("publication_date"), rs.getInt("author_id"));
+    //         }
+    //     } catch (SQLException e) {
+    //         e.printStackTrace();
+    //     }
+    //     return null;
+    // }
+
+}
